2e0b67e2432596c483bcb2deef0d2b6ffc640007
jazchugomez125@gmail.com <jazchugomez125@gmail.com>
2022-10-03 20:51:26 -0300

    se agregó la palabra clave var

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//#region 
+var nombre="jazmin";
+let nombre2='jano';
+const nombre3=´avril´;
+//#endregion
+//#region 
+console.log(nombre);
+console.log(nombre2);
+console.log(nombre3);
+//#endregion
