1f9bb1381587c469fb03df1e3bb92c7f0a5d25be
jazchugomez125@gmail.com <jazchugomez125@gmail.com>
2022-10-05 16:17:12 -0300

    se agrego la variable var

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 //#region 
 var nombre="jazmin";
 let nombre2='jano';
-const nombre3=´avril´;
+const nombre3=`avril`;
 //#endregion
 //#region 
 console.log(nombre);
 console.log(nombre2);
 console.log(nombre3);
+//#endregion
+//#region 
+var inicio=0;
+var fin=[10];
+for ( inicio of fin){
+console.log(nombre);
+console.log(nombre2);
+console.log(nombre3);
+}
 //#endregion
